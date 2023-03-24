56b66b47e576a98407a78ca6553ca97ae2f2c558
Noa Lerch <noazlerch@gmail.com>
2023-03-24 15:48:29 +0100

    working on demo

--- tfocs_python/ConicDemo.py
+++ tfocs_python/ConicDemo.py
@@ -2,18 +2,34 @@ import ConicSolver
 import numpy as np
 import ConicSolver as CS
 
+# Test solver on simple unconstrained quadratic function
 def TestQuadratic():
 	N = 100
 	c = np.random.randn(N, 1)
+	print(c.shape)
 
 	D0 = np.random.randn(N, N)
+
 	D = D0 * np.transpose(D0) + 0.5 * np.identity(N)
 	x_star = - np.divide(D, c)
 	x0 = np.zeros(N)
 
-	f = lambda x: c * x + x * D * x/2  # hopefully correct
-	grad_f = lambda x: c + D * x
-	smooth_func = lambda x, grad: wrapper_objective(f, grad_f, x, grad)
+	# almost certainly not correct dude what
+	# f = lambda x: c * x + x * D * x/2  # hopefully correct
+	f = lambda x: np.transpose(c) * x + np.transpose(x) * D * x/2  # hopefully correct
+	def fun(x):
+		print(x)
+		print(x.shape)
+		print(c.shape)  # shape (1, 100)
+		print(D.shape)
+		# print(np.transpose(c) * x)
+		return f(x)
+
+	def grad_f(x):
+		c + D * x
+
+	def smooth_func(x, grad):
+		wrapper_objective(fun, grad_f, x, grad)
 
 	# problem: x0 is only 0s
 	# affine and projector are both null
@@ -27,8 +43,9 @@ def TestQuadratic():
 
 
 # unsure
-def wrapper_objective(f, g, x, grad=0):  # we just set it to return gradient by default
+def wrapper_objective(f, g, x, grad=0):  # we just set it to NOT return gradient by default
 	if grad:
+		print("x value: " + str(x))
 		return f(x), g(x)
 	else:
 		return f(x), None
